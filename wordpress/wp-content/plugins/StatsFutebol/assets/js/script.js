645cbe0c05f9f08c6e13a87927eb380781894f1e
ramnunes14 <2118823@student.uma.pt>
2025-02-18 13:50:42 +0000

    Update script.js

--- wordpress/wp-content/plugins/StatsFutebol/assets/js/script.js
+++
@@ -1,9 +0,0 @@
-// Código para abrir e fechar o menu
-document.getElementById("menu-toggle").addEventListener("click", function() {
-    const menu = document.getElementById("me");
-    if (menu.style.display === "block") {
-        menu.style.display = "none"; 
-    } else {
-        menu.style.display = "block"; 
-    }
-});
